ed86709006353b8318e8c31e560ddffb33dbea66
rom201 <kovalchukr1104@gmail.com>
2023-03-19 11:46:30 -0400

    added comment to controller

--- src/main/java/com/cydeo/controller/LoginController.java
+++ src/main/java/com/cydeo/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = {"/login","/"})
+    @RequestMapping(value = {"/login","/"})  //localhost:8080/  on localhost:8080/login - same place
     public String login(){
 
         return "login";
