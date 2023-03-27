b8db8e992177955161d96ea49bace92518d7575c
rom201 <kovalchukr1104@gmail.com>
2023-03-26 22:05:16 -0400

    UserDTO converter added

---
+++ src/main/java/com/cydeo/convertor/UserDTOConverter.java
@@ -0,0 +1,26 @@
+package com.cydeo.convertor;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.UserService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class UserDTOConverter implements Converter<String, UserDTO> {
+
+    UserService userService;
+
+    public UserDTOConverter(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDTO convert(String source) {
+        return userService.findById(source);
+    }
+}
+
+
