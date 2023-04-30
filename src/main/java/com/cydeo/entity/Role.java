9f478021f4925c2fd50fa994775d556b84303e02
rom201 <kovalchukr1104@gmail.com>
2023-04-30 11:51:55 -0400

    aoolication property updated, user and roles mapped

--- src/main/java/com/cydeo/entity/Role.java
+++ src/main/java/com/cydeo/entity/Role.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Role {
+@Entity
+@Table(name="roles")
+public class Role extends BaseEntity{
 
-    private Long id;
     private String description;
 
 
--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -4,11 +4,14 @@ import com.cydeo.enums.Gender;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "users")
 public class User extends BaseEntity{
 
     private String firstName;
@@ -17,18 +20,11 @@ public class User extends BaseEntity{
     private String password;
     private boolean enabled;
     private String phone;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+    @ManyToOne
+    @JoinColumn(name = "role_id")
     private Role role;
 
-    public User(Long id, LocalDateTime insertDateTime, Long insertUserId, LocalDateTime lastUpdateDateTime, Long lastUpdateUserId, String firstName, String lastName, String userName, String password, boolean enabled, String phone, Gender gender, Role role) {
-        super(id, insertDateTime, insertUserId, lastUpdateDateTime, lastUpdateUserId);
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.password = password;
-        this.enabled = enabled;
-        this.phone = phone;
-        this.gender = gender;
-        this.role = role;
-    }
+
 }
