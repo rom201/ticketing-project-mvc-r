a096171cb69cd772aa01da3f8f2c798ee4068116
rom201 <kovalchukr1104@gmail.com>
2023-05-16 21:49:31 -0400

    security manual user create completed

--- src/main/java/com/cydeo/TicketingProjectMvcRApplication.java
+++ src/main/java/com/cydeo/TicketingProjectMvcRApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class TicketingProjectMvcRApplication {
@@ -19,6 +21,10 @@ public class TicketingProjectMvcRApplication {
     }
 
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 
 }
